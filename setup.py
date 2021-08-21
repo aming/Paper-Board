1468486b3dc8e98db4c77f1b1bd45bfeb4da6dac
aming <cyberming@gmail.com>
2021-08-20 22:22:36 -0700

    Move the setuptools to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,43 +1,5 @@
+#!/usr/bin/env python
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as file:
-    README = file.read()
-
-setuptools.setup(
-    name="paper-board",
-    version="0.0.1",
-    description="E-Paper board display library",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    author="AMing",
-    author_email="cyberming@gmail.com",
-    url="https://github.com/aming/paper-board",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    license='MIT',
-    packages=setuptools.find_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'paper-board=paper_board.command_line:main',
-            'pull-data=paper_board.data_puller:main',
-            'render-board=paper_board.data_puller:render',
-        ]
-    },
-    install_requires=[
-        'pillow',
-        'pyowm',
-        'yfinance',
-    ],
-    extras_require={
-        'spi':  [
-            "spidev",
-            'rpi.gpio',
-        ]
-    },
-    python_requires=">=3.7",
-)
+if __name__ == "__main__":
+    setuptools.setup()
